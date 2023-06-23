Fixed epoch calculation

epoch.py
@@ -9,11 +9,12 @@ def train_epoch(model, dataloader, optimizer, criterion, device):
     correct_predictions = 0.0
     total_predictions = 0.0
     progress_bar = tqdm(dataloader, desc="Train", dynamic_ncols=True)
+
     for batch in progress_bar:
         audio_values = batch["audio_values"].to(device)
         speaker_ids = batch["speaker_ids"].to(device)
-        optimizer.zero_grad()
 
+        optimizer.zero_grad()
         # Your model forward pass
         outputs = model(audio_values)
 
@@ -21,8 +22,8 @@ def train_epoch(model, dataloader, optimizer, criterion, device):
         loss = criterion(outputs, speaker_ids)
 
         loss.backward()
-
         optimizer.step()
+
         running_loss += loss.item() * audio_values.size(0)
 
         # Compute accuracy
@@ -30,7 +31,7 @@ def train_epoch(model, dataloader, optimizer, criterion, device):
         total_predictions += speaker_ids.size(0)
         correct_predictions += (predicted == speaker_ids).sum().item()
 
-        progress_bar.set_postfix({'loss': '{:.3f}'.format(loss.item() / len(batch))})
+        progress_bar.set_postfix({'loss': '{:.3f}'.format(loss.item())})
 
     epoch_loss = running_loss / len(dataloader.dataset)
     accuracy = correct_predictions / total_predictions
@@ -43,12 +44,15 @@ def validate_epoch(model, dataloader, criterion, device):
     correct_predictions = 0.0
     total_predictions = 0.0
     progress_bar = tqdm(dataloader, desc="Validate", dynamic_ncols=True)
+
     with torch.no_grad():
         for batch in progress_bar:
             audio_values = batch["audio_values"].to(device)
             speaker_ids = batch["speaker_ids"].to(device)
+
             # Your model forward pass
             outputs = model(audio_values)
+
             # compute loss
             loss = criterion(outputs, speaker_ids)
             running_loss += loss.item() * audio_values.size(0)
@@ -58,7 +62,7 @@ def validate_epoch(model, dataloader, criterion, device):
             total_predictions += speaker_ids.size(0)
             correct_predictions += (predicted == speaker_ids).sum().item()
 
-            progress_bar.set_postfix({'loss': '{:.3f}'.format(loss.item() / len(batch))})
+            progress_bar.set_postfix({'loss': '{:.3f}'.format(loss.item())})
 
     epoch_loss = running_loss / len(dataloader.dataset)
     accuracy = correct_predictions / total_predictions